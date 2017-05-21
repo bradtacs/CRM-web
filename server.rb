da044fc11ce13377f144de5f84e7b0105117c206
Zachary Viggiani <zach@bigmojo.net>
2017-05-21 14:20:39 -0400

    Contcts setup fake info.

---
+++ server.rb
@@ -0,0 +1,29 @@
+require './film.rb'
+require 'sinatra'
+require 'pry'
+
+get '/' do
+  erb :index
+end
+
+get '/new_film' do
+  erb :form
+end
+
+get '/films' do
+  "GET request: #{params}"
+end
+
+
+post '/films' do
+  Film.create(params)
+  redirect to('/')
+end
+
+put '/films' do
+  "PUT request: #{params}"
+end
+
+delete '/films' do
+  "DELETE request: #{params}"
+end
---
+++ tables.rb
@@ -0,0 +1,6 @@
+CREATE TABLE user(id integer, first_name text, last_name text);
+INSERT into usertwo (first_name) VALUES ("john");
+
+SELECT * FROM usertwo;
+
+.schema usertwo
